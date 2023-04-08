84816aecce25dabd4dd6bba9ca29ee929f72f6c0
Viktor Shishkalov <viktorshish@gmail.com>
2023-04-08 23:07:08 +0300

    Декомпозиция функции  send_drug_info

--- utils.py
+++ utils.py
@@ -1,10 +1,10 @@
-drugs = [
+DRUGS = [
     {
         'name': 'Аквалор беби',
         'brand': 'АКВАЛОР',
         'variants': [
             {'option': '150 мл мягкий душ', 'price': 449.00},
-            {'option': '15 мл капли', 'price': 237.00}
+            {'option': '15 мл капли', 'price': 237.00},
         ]
     },
     {
@@ -12,21 +12,23 @@ drugs = [
         'brand': 'Фармстандарт-Уфимский витаминный завод,ОАО',
         'variants': [
             {'option': 'раствор для внутримышечного введения 2 мл ампулы 5 шт.', 'price': 234.00},
-            {'option': 'раствор для внутримышечного введения 2 мл ампулы 10 шт.', 'price': 330.00}
+            # {'option': 'раствор для внутримышечного введения 2 мл ампулы 10 шт.', 'price': 330.00},
         ]
     }
 ]
+
+
 def get_user_input(update, context):
-    user_drug = update.message.text.lower().strip()
+    search_query = update.message.text.lower().strip()
     
-    return user_drug
+    return search_query
 
 
-def search_drug(user_drug):
+def search_drug(search_query):
     matches_drugs = []
     
-    for drug in drugs:
-        if user_drug in drug['name'].lower():
+    for drug in DRUGS:
+        if search_query in drug['name'].lower():
             matches_drugs.append(drug)
             
     return matches_drugs
@@ -35,21 +37,35 @@ def search_drug(user_drug):
 def send_drug_info(update, matches_drugs):
     if not matches_drugs:
         update.message.reply_text("К сожалению, я не нашел лекарств по вашему запросу.")
-        return
-    
-    message = ""
-    
+
     for drug in matches_drugs:
-        message += f"Название: {drug['name']}\n"
-        message += f"Производитель: {drug['brand']}\n"
-        message += "Варианты:\n"
-        
-        if len(drug['variants']) == 1:
-            message += f"{drug['variants'][0]['option']} - {drug['variants'][0]['price']} руб.\n"
-        else:
-            for i, variant in enumerate(drug['variants'], start=1):
-                message += f"{i}. {variant['option']} - {variant['price']} руб.\n"
-        
-        message += "\n"
+        update.message.reply_text(build_message(drug))
+
+
+def build_message(drug):
+    message = f"Название: {drug['name']}\nПроизводитель: {drug['brand']}\nВарианты:\n"
+
+    if len(drug['variants']) == 1:
+        message += format_variant(drug['variants'][0])
+    else:
+        message += build_variant_list(drug['variants'])
+
+    message += "\n"
+
+    return message
+
+
+def build_variant_list(variants):
+    variants_list = ""
+
+    for item, variant in enumerate(variants, start=1):
+        variants_list += format_variant(variant, item) + "\n"
+
+    return variants_list
+
+
+def format_variant(variant, item_number=None):
+    if item_number is None:
+        return f"{variant['option']} - {variant['price']} руб."
     
-    update.message.reply_text(message)
+    return f"{item_number}. {variant['option']} - {variant['price']} руб."
