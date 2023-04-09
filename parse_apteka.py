1928bdfa00f2270d9c8837399b732599a0eeda60
Alexey Chernykh <zrivkopen@gmail.com>
2023-04-09 07:02:13 +0300

    исправил замечания, добавил прокси

--- parse_apteka.py
+++ parse_apteka.py
@@ -6,23 +6,30 @@ def getting_category_from_the_user():
     return category_api_link
 
 
-def receive_data_from_the_pharmacy(category_api_link):
+def get_proxies():
+    with open('file.txt', 'r') as f:
+        proxies = {'http': f.readline().strip(), 'https': f.readline().strip()}
+    return proxies
+
+
+def receive_pharmacy_pages(category_api_link):
     headers = {
-        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.84 Safari/537.36',
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36',
     }
 
-    response = requests.get(category_api_link, headers=headers)
+    proxies = get_proxies()
+
+    response = requests.get(category_api_link, headers=headers, proxies=proxies)
     sum_of_page_medicine_list = response.json()["totalCount"] // response.json()["currentCount"]
     params = {}
-    for page in range(sum_of_page_medicine_list + 1):
-        params[page] = page
-
-    response_with_params = requests.get(category_api_link, headers=headers, params=params)
-
+    for page in range(sum_of_page_medicine_list+1):
+        params = {'page': f'page={page}'}
+        response_with_params = requests.get(category_api_link.format(params['page']), headers=headers, proxies=proxies)
+        print(response_with_params.url)
     return response_with_params
 
 
-def processing_data_from_the_pharmacy(response_with_params):
+def processing_data_pharmacy(response_with_params):
     medicine_list = response_with_params.json()["result"]
     for medicine in medicine_list:
         title = medicine["tradeName"]
@@ -37,7 +44,7 @@ def processing_data_from_the_pharmacy(response_with_params):
     return medicine_list
 
 
-def data_output_from_the_pharmacy(category_api_link, response_with_params):
+def output_data_pharmacy(category_api_link, response_with_params):
     receive_data_from_the_pharmacy(category_api_link)
     processing_data_from_the_pharmacy(response_with_params)
 
