ca4b9ef9e8897395f4c988e3e441c11bb691c52e
Viktor Shishkalov <viktorshish@gmail.com>
2023-04-13 00:03:39 +0300

    add get responce geo

--- new_pars_pharmacies.py
+++ new_pars_pharmacies.py
@@ -1,4 +1,3 @@
-import json
 import requests
 
 
@@ -16,10 +15,22 @@ def create_cities_dict(cities):
 
     return city_dict
 
+def get_responce_pharmacies(url_geo):
+    result_pharmacies_cities = {}
+for city in id_cities:
+    url = f'https://api.garzdrav.ru:2872/Retails/{city}'
+    response = requests.get(url)
+    result_pharmacies_cities[id_cities[city]] = response.json()
+    response = requests.get(url_geo)
+
+    json_geo = response.json()
+
+    print(json_geo)
+
 
 if __name__ == '__main__':
     url = 'https://aptekalegko.ru/_next/data/SY0uI5MoAnmSdSWosg1JN/catalog/oftalmologiya/zabolevaniya-glaz.json'
-   
+    rl_geo = 
     cities = get_responce_cities(url)
 
     cities_dict = create_cities_dict(cities)
