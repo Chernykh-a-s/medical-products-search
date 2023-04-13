fcfc0bec21a49f4fcfdff91166c5ee206b383412
Viktor Shishkalov <viktorshish@gmail.com>
2023-04-13 16:00:16 +0300

    Фильтрация итогового json по аптекам

--- new_pars_pharmacies.py
+++ new_pars_pharmacies.py
@@ -1,4 +1,5 @@
 import requests
+import json
 
 
 def get_responce_cities(url):
@@ -9,28 +10,46 @@ def get_responce_cities(url):
 
 def create_cities_dict(cities):
     city_dict = {}
-
     for city in cities:
         city_dict[city['guid']] = city['title']
 
     return city_dict
 
-def get_responce_pharmacies(url_geo):
-    result_pharmacies_cities = {}
-for city in id_cities:
-    url = f'https://api.garzdrav.ru:2872/Retails/{city}'
-    response = requests.get(url_geo)
-    result_pharmacies_cities[id_cities[city]] = response.json()
-  
-    json_geo = response.json()
 
-    print(json_geo)
+def get_pharmacies_by_city(cities_dict):
+    
+    result_filtered = {}
+    for city_id, city_name in cities_dict.items():
+        url = f'https://api.garzdrav.ru:2872/Retails/{city_id}'
+        response = requests.get(url)
+        pharmacies = response.json()
+
+        result_filtered[city_name] = []
+        for pharmacy in pharmacies:
+            result_filtered[city_name].append({
+                "city": pharmacy["city"],
+                "brand": pharmacy["brand"],
+                "street": pharmacy["street"],
+                "buildNumber": pharmacy["buildNumber"],
+                "weekDayTime": pharmacy["weekDayTime"],
+                "phone": pharmacy["phone"],
+                "coordinates": pharmacy["coordinates"]
+            })
+
+    with open('result_filtered.json', 'w', encoding='utf-8') as f:
+        json.dump(result_filtered, f, ensure_ascii=False, indent=4)
+
+    return result_filtered
 
 
 if __name__ == '__main__':
     url = 'https://aptekalegko.ru/_next/data/SY0uI5MoAnmSdSWosg1JN/catalog/oftalmologiya/zabolevaniya-glaz.json'
-    rl_geo = 
+   
     cities = get_responce_cities(url)
 
     cities_dict = create_cities_dict(cities)
+
+    result_pharmacies_cities = get_pharmacies_by_city(cities_dict)
+
+    
    
