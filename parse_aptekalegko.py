ec6d32d0ea7b3c6002ee39293e8cd8c23583dd5f
Alexey Chernykh <zrivkopen@gmail.com>
2023-04-15 03:56:50 +0300

    bug fixed parse_aptekalegko.py

--- parse_aptekalegko.py
+++ parse_aptekalegko.py
@@ -1,120 +1,99 @@
 import requests
-from bs4 import BeautifulSoup
 import json
 import time
+import configparser
 
 
-def get_proxies():
-    with open('proxy.txt', 'r') as f:
-        proxies = {'http': f.readline().strip(), 'https': f.readline().strip()}
-    return proxies
+def get_proxy_from_settings():
+    config = configparser.ConfigParser()
+    config.read("settings.ini")
+    return {'http': config["Proxy"]["proxy"], 'https': config["Proxy"]["proxy"]}
 
-def get_ip():
-    with open('ip.txt', 'r') as f:
-        ip = f.readline().strip()
-    return ip
 
-def make_request(url):
+def get_ip_from_settings():
+    config = configparser.ConfigParser()
+    config.read("settings.ini")
+    return config["Proxy"]["ip"]
+
+
+def make_request_with_proxy(url, params):
     headers = {
         'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.84 Safari/537.36',
     }
-    proxies = get_proxies()
-    response = requests.get(url=url, headers=headers, proxies=proxies)
+    proxies = get_proxy_from_settings()
+    response = requests.get(url=url, headers=headers, proxies=proxies, params=params)
     return response
 
 
-def receive_category_links(response):
-
-    soup = BeautifulSoup(response.text, 'html.parser')
-
-    list_data_pharmacy = soup.find('ul', '_sectionBadLekarstva_ul__DQ4zv').find_all('a')
-
-    category_links = {}
-
-    for link in list_data_pharmacy:
-        url = link.get("href")
-        text = link.text
-        category_links[text] = f'https://aptekalegko.ru/{url}'
-
-    return category_links
-
-def receive_subcategory_links(response):
-
-    soup = BeautifulSoup(response.text, 'html.parser')
-
-    list_data_pharmacy = soup.find('ul', '_catalogLvl2_ul__xJ8Jo').find_all('a')
-
-    subcategory_links = {}
-
-    for link in list_data_pharmacy:
-        url = link.get("href")
-        text = link.text
-        subcategory_links[text] = f'https://aptekalegko.ru/{url}'
-
-    return subcategory_links
-
-def write_category_links_in_json(file_name, category_links):
-
-    with open(file_name, 'a', encoding='utf-8') as file:
-        json.dump(category_links, file, indent=4, ensure_ascii=False)
+def load_json_with_pharmacy_categories():
+    with open('category_aptekaleg.json', 'r', encoding='utf-8') as f:
+        return json.load(f)
 
 
-def write_subcategory_links_in_json():
-    with open('category_links.json', encoding="utf-8") as fh:
-        data = json.loads(fh.read())
-
-    file_name = 'subcategory_links.json'
-
-    for category_name, url in data.items():
-        response = make_request(url)
-        subcategory_links = receive_subcategory_links(response)
-        write_category_links_in_json(file_name,  subcategory_links)
-        time.sleep(7)
+def receive_category_from_json(json_file):
+    pharmacy_categories = []
+    for name_of_category, category_for_parse in json_file_with_apteka_categories.items():
+        for name_of_category in category_for_parse:
+            category_subcategory_for_url = str(name_of_category['category']) + '/' + str(name_of_category[
+                                                                                             'subcategory'])
+            pharmacy_categories.append(category_subcategory_for_url)
+    return pharmacy_categories
 
 
-def parse_api():
-    data_for_parse = {
-        'category': 'oftalmologiya',
-        'subcategory': 'zabolevaniya-glaz',
+def parse_apteka(pharmacy_categories, json_file_with_apteka_categories):
+
+    ip = get_ip_from_settings()
+
+    url = 'https://aptekalegko.ru/_next/data/SY0uI5MoAnmSdSWosg1JN/catalog/{}.json'
+    params = {
+        'pages': None,
+        'order': 'None',
+        'ipClient': ip,
+        'cityGuid': 'c073f480-6d97-4af3-976b-3c069f39db52',
+        'catalogLvl2': None,
+        'catalogLvl3': None
     }
 
-    sum_of_pages = 13
-    ip = get_ip()
+    for page in range(1):
+        params['pages'] = page + 1
 
-    address_template_api = 'https://aptekalegko.ru/_next/data/FUDDCaS8piyfZMougusFc/catalog/{}' \
-                           '/{}.json?pages=&order=None&ipClient={}&cityGuid=c073f480-6d97' \
-                           '-4af3-976b-3c069f39db52&catalogLvl2={}&catalogLvl3={}'.format(
-        data_for_parse['category'], data_for_parse['subcategory'], ip, data_for_parse['category'], data_for_parse[
-            'subcategory'])
+    urls_category = []
+    for category in pharmacy_categories:
+        url_format = url.replace('{}', category)
+        urls_category.append(url_format)
+        print(urls_category)
 
-    links_api_for_parse = {}
+    for category in urls_category:
+        response = make_request_with_proxy(category, params=params)
+        try:
+            count_of_pages = response.json()["pageProps"]["products"]["count"] // len(response.json()["pageProps"][
+                                                                                          "products"]["products"]) + 1
+        except ZeroDivisionError:
+            continue
 
-    for page in range(sum_of_pages):
-        link = address_template_api.replace('pages=', 'pages='+str(page+1))
-        links_api_for_parse[f'page: {page+1}'] = link
+        for page in range(count_of_pages):
+            params['pages'] = page + 1
+            response = make_request_with_proxy(category, params=params)
+            medicine_list = response.json()["pageProps"]["products"]["products"]
 
-    drugs = []
-    one_position_of_product = {}
+            for product in medicine_list:
+                one_position_of_product = {}
+                product_name = product["product"]
+                manufacturer = product["manufacturer"]
+                price = product["price"]
+                one_position_of_product = {
+                    'product_name': product_name,
+                    'manufacturer': manufacturer,
+                    'price': price,
+                }
 
-    for page, link in links_api_for_parse.items():
-        response = make_request(link)
-        medicine_list = response.json()["pageProps"]["products"]["products"]
-        time.sleep(5)
-        for product in medicine_list:
-            one_position_of_product = {}
-            product_name = product["product"]
-            manufacturer = product["manufacturer"]
-            price = product["price"]
-            one_position_of_product = {
-                'product_name': product_name,
-                'manufacturer': manufacturer,
-                'price': price,
-            }
-            drugs.append(one_position_of_product)
-
-    with open("drugs.json", "a", encoding='utf-8') as f:
-        json.dump(drugs, f, indent=4, ensure_ascii=False)
+                with open("drugs.json", "a", encoding='utf-8') as f:
+                    json.dump(one_position_of_product, f, indent=4, ensure_ascii=False)
+                    f.write(','), f.write('\n')
 
 
 if __name__ == '__main__':
-    get_links_from_jsons_for_parse_api()
+    json_file_with_apteka_categories = load_json_with_pharmacy_categories()
+    receive_category_from_json(json_file_with_apteka_categories)
+    pharmacy_categories = receive_category_from_json(json_file_with_apteka_categories)
+    parse_apteka(pharmacy_categories, json_file_with_apteka_categories)
